f18089e08c9485b1f0c331c9ef565d8cc1ed7020
Benno <57860196+bennoinbeta@users.noreply.github.com>
2024-07-08 07:58:24 +0200

    #develop wip petstore example

---
+++ examples/openapi-router/express/petstore/src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+import { router } from './router';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/*', router);
+
+app.listen(port, () => {
+	console.log(`Server is running on http://localhost:${port}`);
+});
