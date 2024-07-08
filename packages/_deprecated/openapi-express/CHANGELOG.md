0cb2bbf40cfc4ddfd8e7cf9bc0f47a39aa74db5a
Benno <57860196+bennoinbeta@users.noreply.github.com>
2024-07-08 07:08:02 +0200

    #42 fixed publish:patch npm script

---
+++ packages/_deprecated/openapi-express/CHANGELOG.md
@@ -0,0 +1,39 @@
+# openapi-express
+
+## 0.0.7
+
+### Patch Changes
+
+- [#39](https://github.com/inbeta-group/monorepo/pull/39) [`4dd985a`](https://github.com/inbeta-group/monorepo/commit/4dd985a432a4197324792cb25d7df3c0f9ccc912) Thanks [@bennoinbeta](https://github.com/bennoinbeta)! - updated dependencies
+
+## 0.0.6
+
+### Patch Changes
+
+- [#35](https://github.com/inbeta-group/monorepo/pull/35) [`2b5eaac`](https://github.com/inbeta-group/monorepo/commit/2b5eaacff65850dfb473c376f3907aff85f613a9) Thanks [@github-actions](https://github.com/apps/github-actions)! - fixed typos
+
+## 0.0.5
+
+### Patch Changes
+
+- [#26](https://github.com/inbeta-group/monorepo/pull/26) [`08b5aea`](https://github.com/inbeta-group/monorepo/commit/08b5aea3ff12687fec6d7a35a394d6945a5bc408) Thanks [@github-actions](https://github.com/apps/github-actions)! - Updated README
+
+## 0.0.4
+
+### Patch Changes
+
+- [`4b8fe68`](https://github.com/inbeta-group/monorepo/commit/4b8fe685450181f2f3eefca0a88597c591d6c3e4) Thanks [@bennoinbeta](https://github.com/bennoinbeta)! - Updated README
+
+## 0.0.3
+
+### Patch Changes
+
+- [#20](https://github.com/inbeta-group/monorepo/pull/20) [`29cbda7`](https://github.com/inbeta-group/monorepo/commit/29cbda7ba2e98d626d6c5c38ded4c61e236dd019) Thanks [@github-actions](https://github.com/apps/github-actions)! - Updated README
+
+- [#17](https://github.com/inbeta-group/monorepo/pull/17) [`a404128`](https://github.com/inbeta-group/monorepo/commit/a404128d187354ea60ac788a62171f15686ddcb0) Thanks [@bennoinbeta](https://github.com/bennoinbeta)! - Deploy CI test
+
+## 0.0.2
+
+### Patch Changes
+
+- [`818b35c`](https://github.com/inbeta-group/monorepo/commit/818b35c51ad86dbd56b819e8c23551d328f0c131) Thanks [@bennoinbeta](https://github.com/bennoinbeta)! - Deploy CI test
---
+++ packages/_deprecated/openapi-express/src/types/validation.ts
@@ -0,0 +1,44 @@
+import type { TOperationPathParams, TOperationQueryParams, TRequestBody } from '@ibg/types/openapi';
+
+import type { TParserSchema } from './parser';
+
+// =============================================================================
+// Validation Schema
+// =============================================================================
+
+// Validation schema for query parameters
+export type TOpenApiQueryParamsValidationSchema<GPathOperation> =
+	undefined extends TOperationQueryParams<GPathOperation> // If the queryParams can be undefined/optional
+		? { querySchema: Required<TParserSchema<TOperationQueryParams<GPathOperation> | undefined>> }
+		: TOperationQueryParams<GPathOperation> extends never
+			? { querySchema?: TBaseValidationSchemaEntry }
+			: { querySchema: TParserSchema<TOperationQueryParams<GPathOperation>> };
+
+// Validation schema for path parameters
+export type TOpenApiPathParamsValidationSchema<GPathOperation> =
+	undefined extends TOperationPathParams<GPathOperation> // If the pathParams can be undefined/optional
+		? { pathSchema: TParserSchema<TOperationPathParams<GPathOperation> | undefined> }
+		: TOperationPathParams<GPathOperation> extends never
+			? { pathSchema?: TBaseValidationSchemaEntry }
+			: { pathSchema: TParserSchema<TOperationPathParams<GPathOperation>> };
+
+// Validation schema for body
+export type TOpenApiBodyValidationSchema<GPathOperation> =
+	undefined extends TRequestBody<GPathOperation> // If the body can be undefined/optional
+		? { bodySchema: TParserSchema<TRequestBody<GPathOperation> | undefined> }
+		: TRequestBody<GPathOperation> extends never
+			? { bodySchema?: TBaseValidationSchemaEntry }
+			: { bodySchema: TParserSchema<TRequestBody<GPathOperation>> };
+
+export type TOpenApiValidationSchema<GPathOperation> =
+	TOpenApiPathParamsValidationSchema<GPathOperation> &
+		TOpenApiQueryParamsValidationSchema<GPathOperation> &
+		TOpenApiBodyValidationSchema<GPathOperation>;
+
+export interface TBaseValidationSchema {
+	querySchema?: TBaseValidationSchemaEntry;
+	pathSchema?: TBaseValidationSchemaEntry;
+	bodySchema?: TBaseValidationSchemaEntry;
+}
+
+export type TBaseValidationSchemaEntry = Record<string, TParserSchema<any>>;
