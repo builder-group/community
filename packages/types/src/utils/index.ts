0f715ef804479f2abbec4c1835341fbf43bd08ae
Benno <57860196+bennoinbeta@users.noreply.github.com>
2024-07-10 07:44:50 +0200

    #48 fixed typo

--- packages/types/src/utils/index.ts
+++ packages/types/src/utils/index.ts
@@ -6,10 +6,10 @@ export type TUnarray<T> = T extends (infer U)[] ? U : T;
 export type TUnwrap<T> = T extends Promise<infer U> ? U : T;
 
 // https://fettblog.eu/typescript-union-to-intersection/
-export type TUnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (
-	k: infer I
-) => void
-	? I
+export type TUnionToIntersection<T> = (T extends any ? (x: T) => any : never) extends (
+	x: infer R
+) => any
+	? R
 	: never;
 
 // https://github.com/Microsoft/TypeScript/issues/25760
@@ -26,26 +26,16 @@ export type TEqual<X, Y> =
 
 export type TNotEqual<X, Y> = true extends TEqual<X, Y> ? false : true;
 
-/**
- * Useful to flatten the type output to improve type hints shown in editors. And also to transform an interface into a type to aide with assignability.
- *
- * Copyright: From sindresorhus/type-fest
- */
-export type TSimplify<T> = { [KeyType in keyof T]: T[KeyType] } & {};
-
-export type TInterfaceToType<T> = T extends Function
-	? T
-	: { [K in keyof T]: TInterfaceToType<T[K]> };
-
-export type TRequiredKeysOf<BaseType extends object> = Exclude<
+export type TRequiredKeysOf<GBaseType extends object> = Exclude<
 	{
-		[Key in keyof BaseType]: BaseType extends Record<Key, BaseType[Key]> ? Key : never;
-	}[keyof BaseType],
+		[Key in keyof GBaseType]: GBaseType extends Record<Key, GBaseType[Key]> ? Key : never;
+	}[keyof GBaseType],
 	undefined
 >;
 
-export type THasRequiredKeys<BaseType extends object> =
-	TRequiredKeysOf<BaseType> extends never ? false : true;
+/** Does this object contain required keys? */
+export type THasRequiredKeys<GBaseType extends object> =
+	TRequiredKeysOf<GBaseType> extends never ? false : true;
 
 export type TIsAny<T> = boolean extends (T extends never ? true : false) ? true : false;
 
