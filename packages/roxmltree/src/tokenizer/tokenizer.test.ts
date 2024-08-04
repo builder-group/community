bb210e27d58f71efbc6482acf516709249420b55
Benno <57860196+bennoinbeta@users.noreply.github.com>
2024-08-04 18:53:28 +0200

    #develop wip test setup for roxmltree

--- packages/roxmltree/src/tokenizer/tokenizer.test.ts
+++ packages/roxmltree/src/tokenizer/tokenizer.test.ts
@@ -1,15 +1,61 @@
-import { describe, it } from 'vitest';
+import { describe, expect, it } from 'vitest';
 
-const xmlString = `
-<root>
-  <child attr="value">Content</child>
-  <!-- Comment -->
-  <![CDATA[Some <CDATA> content]]>
-</root>
-`;
+import { parse } from './index';
+import { TToken, TXmlEvents } from './types';
 
-describe('tokenizer tests', () => {
-	it('should work', () => {
-		// TODO
+class EventsCollector implements TXmlEvents {
+	private _tokens: TToken[] = [];
+
+	public get tokens(): TToken[] {
+		return this._tokens;
+	}
+
+	public token(token: TToken): void {
+		this._tokens.push(token);
+	}
+}
+
+function collectTokens(text: string): TToken[] {
+	const collector = new EventsCollector();
+	parse(text, true, collector);
+	return collector.tokens;
+}
+
+function assertTokens(text: string, ...tokens: TToken[]) {
+	expect(collectTokens(text)).toBe(tokens);
+}
+
+describe('CDATA', () => {
+	it('cdata_01', () => {
+		assertTokens(
+			'<p><![CDATA[content]]></p>',
+			{
+				type: 'ElementStart',
+				prefix: '',
+				local: 'p',
+				start: 0
+			},
+			{
+				type: 'ElementEnd',
+				end: {
+					type: 'Open'
+				},
+				range: [2, 3]
+			},
+			{
+				type: 'Cdata',
+				text: 'content',
+				range: [3, 22]
+			},
+			{
+				type: 'ElementEnd',
+				end: {
+					type: 'Close',
+					prefix: '',
+					name: 'p'
+				},
+				range: [22, 26]
+			}
+		);
 	});
 });
