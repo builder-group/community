122c3b4e562f386cb1f73c0eacfe4d7b5d42470f
Benno <57860196+bennoinbeta@users.noreply.github.com>
2024-08-10 17:07:48 +0200

    #61 bookstore

--- packages/xml-tokenizer/src/selector/TokenSelector.test.ts
+++ packages/xml-tokenizer/src/selector/TokenSelector.test.ts
@@ -1,7 +1,35 @@
-import { describe, it } from 'vitest';
+import { readFile } from 'node:fs/promises';
+import path from 'node:path';
+import { beforeAll, describe, expect, it } from 'vitest';
+
+import { tokenize, type TXMLToken } from '../tokenizer';
+import { TokenSelector } from './TokenSelector';
 
 describe('selector tests', () => {
+	let bookStoreXml = '';
+
+	beforeAll(async () => {
+		bookStoreXml = await readFile(
+			path.join(__dirname, '../__tests__/resources/bookstore.xml'),
+			'utf-8'
+		);
+	});
+
 	it('should work', () => {
-		// TODO
+		const selector = new TokenSelector([
+			[
+				{ axis: '/', local: 'bookstore' },
+				{ axis: '/', local: 'book', attributes: [{ local: 'category', value: 'COOKING' }] }
+			]
+		]);
+		const recorded: TXMLToken[] = [];
+
+		tokenize(bookStoreXml, false, (token) => {
+			selector.pipeToken(token, (recordedToken) => {
+				recorded.push(recordedToken);
+			});
+		});
+
+		expect(recorded).not.toBeNull();
 	});
 });
