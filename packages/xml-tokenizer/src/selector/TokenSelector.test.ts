42fe8d00fae4a75fdce1ea64045b9ef77fdc77ca
Benno <57860196+bennoinbeta@users.noreply.github.com>
2024-08-11 09:44:15 +0200

    #61 basic test setup

--- packages/xml-tokenizer/src/selector/TokenSelector.test.ts
+++ packages/xml-tokenizer/src/selector/TokenSelector.test.ts
@@ -5,6 +5,7 @@ import { beforeAll, describe, expect, it } from 'vitest';
 import { tokenize, type TXmlToken } from '../tokenizer';
 import { tokensToXml } from '../tokens-to-xml';
 import { TokenSelector } from './TokenSelector';
+import { type TTokenSelectPath } from './types';
 
 describe('selector tests', () => {
 	let bookStoreXml = '';
@@ -16,6 +17,43 @@ describe('selector tests', () => {
 		);
 	});
 
+	it('should match /bookstore', () => {
+		assertSelection(
+			bookStoreXml,
+			[[{ axis: '/', local: 'bookstore' }]],
+			`<bookstore>
+        <book category="COOKING">
+                <title lang="en">Everyday Italian</title>
+                <author>Giada De Laurentiis</author>
+                <year>2005</year>
+                <price>30.00</price>
+        </book>
+        <book category="CHILDREN">
+                <title lang="en">Harry Potter</title>
+                <author>J K. Rowling</author>
+                <year>2005</year>
+                <price>29.99</price>
+        </book>
+        <book category="WEB">
+                <title lang="en">XQuery Kick Start</title>
+                <author>James McGovern</author>
+                <author>Per Bothner</author>
+                <author>Kurt Cagle</author>
+                <author>James Linn</author>
+                <author>Vaidyanathan Nagarajan</author>
+                <year>2003</year>
+                <price>49.99</price>
+        </book>
+        <book category="WEB">
+                <title lang="en">Learning XML</title>
+                <author>Erik T. Ray</author>
+                <year>2003</year>
+                <price>39.95</price>
+        </book>
+</bookstore>`
+		);
+	});
+
 	it('should work', () => {
 		const selector = new TokenSelector([
 			[
@@ -24,7 +62,7 @@ describe('selector tests', () => {
 			],
 			[
 				{ axis: '/', local: 'bookstore' },
-				{ axis: '/', local: 'book', attributes: [{ local: 'category', value: 'WEB' }] }
+				{ axis: '/', local: 'book', attributes: [{ local: 'category', value: 'COOKING' }] }
 			]
 		]);
 		const recorded: TXmlToken[] = [];
@@ -39,3 +77,20 @@ describe('selector tests', () => {
 		expect(recorded).not.toBeNull();
 	});
 });
+
+function collectRecordedTokens(text: string, tokenSelectPaths: TTokenSelectPath[]): TXmlToken[] {
+	const recordedTokens: TXmlToken[] = [];
+	const selector = new TokenSelector(tokenSelectPaths);
+	tokenize(text, false, (token) => {
+		selector.pipeToken(token, (recordedToken) => {
+			recordedTokens.push(recordedToken);
+		});
+	});
+	return recordedTokens;
+}
+
+function assertSelection(text: string, tokenSelectPaths: TTokenSelectPath[], result: string): void {
+	expect(tokensToXml(collectRecordedTokens(text, tokenSelectPaths)).replaceAll(/\s/g, '')).toBe(
+		result.replaceAll(/\s/g, '')
+	);
+}
