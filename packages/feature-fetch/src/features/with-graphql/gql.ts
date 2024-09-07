a3cb614c7694d02b353008d35c24434898daa7cf
Benno <57860196+bennoinbeta@users.noreply.github.com>
2024-09-07 16:54:02 +0200

    #develop fixed typo

--- packages/feature-fetch/src/features/with-graphql/gql.ts
+++ packages/feature-fetch/src/features/with-graphql/gql.ts
@@ -1,16 +1,17 @@
 /* eslint-disable no-param-reassign -- Ok in reduce function */
 
-export function gql(strings: TemplateStringsArray, ...values: unknown[]): string {
+// https://github.com/apollographql/graphql-tag/blob/main/src/index.ts
+export function gql(literals: TemplateStringsArray, ...args: unknown[]): string {
 	// If a single string is passed, return it as is
-	if (strings.raw.length === 1) {
-		return strings.raw[0] ?? '';
+	if (literals.raw.length === 1) {
+		return literals.raw[0] ?? '';
 	}
 
 	// Otherwise, interleave the strings with the interpolated values
-	return strings.raw.reduce((result, string, i) => {
+	return literals.raw.reduce((result, string, i) => {
 		result += string;
-		if (i < values.length) {
-			result += String(values[i]);
+		if (i < args.length) {
+			result += String(args[i]);
 		}
 		return result;
 	}, '');
