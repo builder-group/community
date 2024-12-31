d22b8ebf1b6ef3e62148ddbd880c1fb417896741
Benno <57860196+bennoinbeta@users.noreply.github.com>
2024-12-31 09:23:43 +0100

    #88 moved google-webfonts-client to new feature types

--- packages/google-webfonts-client/src/types.ts
+++ packages/google-webfonts-client/src/types.ts
@@ -1,4 +1,34 @@
+import { FetchError, TApiFeature, TFetchClient, TResult } from 'feature-fetch';
 import { type components } from './gen/v1';
+
+export interface TGoogleWebfontsFeature {
+	key: 'google-webfonts';
+	api: {
+		_apiFetchClient: TFetchClient<[TApiFeature]>;
+		getWebFonts(options?: {
+			family?: TFontFamily;
+			subset?: TFontSubset;
+			capability?: TFontCapability;
+			sort?: TSort;
+		}): Promise<TResult<components['schemas']['WebfontList'], FetchError>>;
+		getFontFileUrl(
+			familiy: TFontFamily,
+			options: {
+				fontWeight?: number;
+				fontStyle?: TFontStyle;
+				capability?: TFontCapability;
+			}
+		): Promise<TResult<string | null, FetchError>>;
+		downloadFontFile(
+			familiy: TFontFamily,
+			options: {
+				fontWeight?: number;
+				fontStyle?: TFontStyle;
+				capability?: TFontCapability;
+			}
+		): Promise<TResult<Uint8Array | null, FetchError>>;
+	};
+}
 
 export type TFontFamily = components['parameters']['Family'];
 export type TFontSubset = components['parameters']['Subset'];
