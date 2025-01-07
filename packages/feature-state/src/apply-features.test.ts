ae29c4ed40263051b02baa0a4f305f7c06b062d4
Benno <57860196+bennoinbeta@users.noreply.github.com>
2025-01-07 08:51:31 +0100

    #develop fixed typo

--- packages/feature-state/src/apply-features.test.ts
+++ packages/feature-state/src/apply-features.test.ts
@@ -6,17 +6,9 @@ import { withMultiUndo, withStorage, withUndo } from './features';
 describe('applyFeatures function', () => {
 	it('should have correct types', () => {
 		const state = createState(0);
-
-		// Empty features array - works!
 		const state1 = applyFeatures(createState(0), []);
-
-		// Single feature - works with correct type chain!
 		const state2 = applyFeatures(createState(0), [(s) => withUndo(s)]);
-
-		// Two features - works with correct type chain!
 		const state3 = applyFeatures(createState(0), [(s) => withUndo(s), (s) => withMultiUndo(s)]);
-
-		// Three features - works with correct type chain!
 		const state4 = applyFeatures(createState(0), [
 			(s) => withUndo(s),
 			(s) => withMultiUndo(s),
