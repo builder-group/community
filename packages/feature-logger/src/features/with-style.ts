ae3f571dab8717002a807da06f6802b01520c25e
Benno <57860196+bennoinbeta@users.noreply.github.com>
2025-03-23 15:38:44 +0100

    #develop fixed typo

--- packages/feature-logger/src/features/with-style.ts
+++ packages/feature-logger/src/features/with-style.ts
@@ -1,31 +1,23 @@
 import { TEnforceFeatureConstraint, TFeatureDefinition } from '@blgc/types/features';
 import { TLoggerMiddleware, TStyleFeature, type TLogger } from '../types';
 
-const DEFAULT_STYLES: Record<string, string> = {
-	trace: 'color: #aaa',
-	debug: 'color: #888',
-	log: 'color: #333',
-	info: 'color: #0066cc',
-	warn: 'color: #f90',
-	error: 'color: #f33'
-};
-
 export function withStyle<GFeatures extends TFeatureDefinition[]>(
 	baseLogger: TEnforceFeatureConstraint<TLogger<GFeatures>, TLogger<GFeatures>, []>,
 	styles: Record<string, string> = {}
 ): TLogger<[TStyleFeature, ...GFeatures]> {
 	(baseLogger as TLogger<[TStyleFeature]>)._features.push('style');
 
-	baseLogger.middlewares.push(styleMiddleware({ ...DEFAULT_STYLES, ...styles }));
+	baseLogger.middlewares.push(styleMiddleware(styles));
 
 	return baseLogger as TLogger<[TStyleFeature, ...GFeatures]>;
 }
 
-export function styleMiddleware(styles: Record<string, string>): TLoggerMiddleware {
+export function styleMiddleware(styles: Record<string, string> = {}): TLoggerMiddleware {
+	const allStyles = { ...DEFAULT_STYLES, ...styles };
 	return (next) => {
 		return (logMethod, data) => {
-			const style = styles[logMethod];
-			if (typeof data[0] === 'string' && style) {
+			const style = allStyles[logMethod];
+			if (typeof data[0] === 'string' && style != null) {
 				data[0] = `%c${data[0]}`;
 				data.splice(1, 0, style);
 			}
@@ -33,3 +25,12 @@ export function styleMiddleware(styles: Record<string, string>): TLoggerMiddlewa
 		};
 	};
 }
+
+export const DEFAULT_STYLES: Record<string, string> = {
+	trace: 'color: #aaa',
+	debug: 'color: #888',
+	log: 'color: #333',
+	info: 'color: #0066cc',
+	warn: 'color: #f90',
+	error: 'color: #f33'
+};
