5223fb101639129326e824f04bdf8bed7a59e02d
Benno <57860196+bennoinbeta@users.noreply.github.com>
2025-09-06 17:12:22 +0200

    #develop cleanup callback for use-listener and use-subscriber

--- packages/feature-react/src/state/hooks/use-listener.ts
+++ packages/feature-react/src/state/hooks/use-listener.ts
@@ -1,21 +1,36 @@
 import { TFeatureDefinition } from '@blgc/types/features';
-import type { TListenerCallback, TListenerOptions, TState } from 'feature-state';
+import type { TListenerContext, TListenerOptions, TState } from 'feature-state';
 import React from 'react';
 
 export function useListener<GValue, GFeatures extends TFeatureDefinition[]>(
 	state: TState<GValue, GFeatures> | null | undefined,
-	callback: TListenerCallback<GValue>,
+	callback: TUseListenerCallback<GValue>,
 	deps: React.DependencyList = [],
 	options: TUseListenerOptions<GValue> = {}
 ): void {
 	const { ...listenerOptions } = options;
 
 	React.useEffect(() => {
-		const unbind = state?.listen(callback, { key: 'use-listener', ...listenerOptions });
+		let cleanup: (() => void) | undefined;
+
+		const unbind = state?.listen(
+			async (cx) => {
+				cleanup?.();
+				const result = await callback(cx);
+				cleanup = typeof result === 'function' ? result : undefined;
+			},
+			{ key: 'use-listener', ...listenerOptions }
+		);
+
 		return () => {
+			cleanup?.();
 			unbind?.();
 		};
 	}, [state, ...deps]);
 }
 
 export interface TUseListenerOptions<GValue> extends TListenerOptions<GValue> {}
+
+export type TUseListenerCallback<GValue> = (
+	context: TListenerContext<GValue>
+) => (() => void) | Promise<() => void>;
--- packages/feature-react/src/state/hooks/use-subscriber.ts
+++ packages/feature-react/src/state/hooks/use-subscriber.ts
@@ -1,21 +1,36 @@
 import { TFeatureDefinition } from '@blgc/types/features';
-import type { TListenerCallback, TListenerOptions, TState } from 'feature-state';
+import type { TListenerContext, TListenerOptions, TState } from 'feature-state';
 import React from 'react';
 
 export function useSubscriber<GValue, GFeatures extends TFeatureDefinition[]>(
 	state: TState<GValue, GFeatures> | null | undefined,
-	callback: TListenerCallback<GValue>,
+	callback: TUseSubscriberCallback<GValue>,
 	deps: React.DependencyList = [],
 	options: TUseSubscriberOptions<GValue> = {}
 ): void {
 	const { ...listenerOptions } = options;
 
 	React.useEffect(() => {
-		const unbind = state?.subscribe(callback, { key: 'use-subscriber', ...listenerOptions });
+		let cleanup: (() => void) | undefined;
+
+		const unbind = state?.subscribe(
+			async (cx) => {
+				cleanup?.();
+				const result = await callback(cx);
+				cleanup = typeof result === 'function' ? result : undefined;
+			},
+			{ key: 'use-subscriber', ...listenerOptions }
+		);
+
 		return () => {
+			cleanup?.();
 			unbind?.();
 		};
 	}, [state, ...deps]);
 }
 
 export interface TUseSubscriberOptions<GValue> extends TListenerOptions<GValue> {}
+
+export type TUseSubscriberCallback<GValue> = (
+	context: TListenerContext<GValue>
+) => (() => void) | Promise<() => void>;
