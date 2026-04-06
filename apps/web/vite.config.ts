32a718e31aac6ff0792a60a8c81cb35997937dba
Benno <57860196+bennoinbeta@users.noreply.github.com>
2026-04-06 12:09:31 +0200

    #118 use native vite tsconfig paths

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,17 +4,16 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
 import { nitro } from 'nitro/vite';
 import { defineConfig } from 'vite';
-import tsConfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
+	resolve: {
+		tsconfigPaths: true
+	},
 	server: {
 		port: 3000
 	},
 	plugins: [
-		tsConfigPaths({
-			projects: ['./tsconfig.json']
-		}),
 		mdx(),
 		tanstackStart({
 			srcDirectory: 'src'
