8792443a71abce7121fbe72d6ceb90c9d17c8768
Benno <57860196+bennoinbeta@users.noreply.github.com>
2026-05-20 09:12:25 +0200

    #104 remove unused hooks

--- packages/feature-react/src/state/hooks/index.ts
+++ packages/feature-react/src/state/hooks/index.ts
@@ -1,7 +1,5 @@
 export * from './use-combined-compute';
 export * from './use-compute';
 export * from './use-feature-state';
-export * from './use-feature-state-with-middleware';
 export * from './use-listener';
-export * from './use-selector';
 export * from './use-subscriber';
