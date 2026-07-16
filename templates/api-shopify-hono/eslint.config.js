8dd0999484aa772987f2572dff29ba9a2ec82116
Benno <57860196+bennobuilder@users.noreply.github.com>
2026-07-16 12:13:15 +0200

    #121 ignore generated template artifacts

--- templates/api-shopify-hono/eslint.config.js
+++ templates/api-shopify-hono/eslint.config.js
@@ -1,7 +1,11 @@
 import library from '@blgc/config/eslint/library';
+import { globalIgnores } from 'eslint/config';
 
 /**
  * @see https://eslint.org/docs/latest/use/configure/configuration-files
  * @type {import("eslint").Linter.Config[]}
  */
-export default [...library];
+export default [
+	...library,
+	globalIgnores(['**/graphql-env.d.ts'], '@template/api-shopify-hono/ignores')
+];
